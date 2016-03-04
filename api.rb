8605b80359a152b0f0d122fdb8e02150a822817c
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-03-04 17:59:44 +0900

    Fix http method and change build directory

--- api.rb
+++ api.rb
@@ -1,17 +1,26 @@
 require 'sinatra'
 require 'jekyll'
+require 'securerandom'
+require 'date'
+
 
 get '/' do
   'Hello world!'
 end
 
-get '/build' do
-  Jekyll::Commands::Build.process({source: "themes/default"})
-  file_name = '_g_20160304_xxx'
-  `tar -zcvf files/#{file_name}.tar.gz files/#{file_name}`
-  "http://localhost:4567/download/#{file_name}.tar.gz"
+post '/build' do
+  file_name = SecureRandom.hex
+  sub_dir = Date.today.to_s
+  Dir.mkdir "public/sites/#{sub_dir}" unless Dir.exist?("public/sites/#{sub_dir}")
+  Jekyll::Commands::Build.process({source: "themes/default", destination: "public/sites/#{sub_dir}/#{file_name}"})
+  `tar -zcvf public/sites/#{sub_dir}/#{file_name}.tar.gz public/sites/#{sub_dir}/#{file_name}`
+  "http://localhost:4567/download/#{sub_dir}/#{file_name}.tar.gz"
+end
+
+post '/test' do
+  "Hello #{params['name']}"
 end
 
 get '/download/:filename' do |filename|
-  send_file "./files/#{filename}", :filename => filename, :type => 'Application/octet-stream'
+  send_file "./public/sites/#{filename}", :filename => filename, :type => 'Application/octet-stream'
 end
