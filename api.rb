5e9ea60f08e1d4b5cbe674bb28005550df035897
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-03-04 16:41:07 +0900

    Fix build process

--- api.rb
+++ api.rb
@@ -1,12 +1,15 @@
 require 'sinatra'
+require 'jekyll'
 
 get '/' do
   'Hello world!'
 end
 
 get '/build' do
-  `./build.sh`
-  'http://localhost:4567/download/_g_20160304_xxx.tar.gz'
+  Jekyll::Commands::Build.process({source: "themes/default"})
+  file_name = '_g_20160304_xxx'
+  `tar -zcvf files/#{file_name}.tar.gz files/#{file_name}`
+  "http://localhost:4567/download/#{file_name}.tar.gz"
 end
 
 get '/download/:filename' do |filename|
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-bundle exec jekyll build --source themes/default --destination files/_g_20160304_xxx
-tar -zcvf files/_g_20160304_xxx.tar.gz files/_g_20160304_xxx
