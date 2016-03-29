6aed904af48aef9a2811cf3c75b4277762476832
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-03-29 19:21:33 +0900

    Fix routing

--- api.rb
+++ api.rb
@@ -15,18 +15,10 @@ post '/build' do
   Builder.run(config: params)
 end
 
-post '/test' do
-  "Hello #{params['name']}"
-end
-
 get '/download/:filename' do |filename|
   send_file "./public/sites/#{filename}", :filename => filename, :type => 'Application/octet-stream'
 end
 
-get '/form' do
+get '/build' do
   erb :form
-end
-
-post '/form' do
-  "You said '#{params[:message]}'"
 end
