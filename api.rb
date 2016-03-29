ece6988025196395f532d194ad3ccc78c833ff54
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-03-29 19:14:02 +0900

    enabled work on docker

--- api.rb
+++ api.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require './lib/builder'
 
+set :bind, '0.0.0.0'
 
 get '/' do
   'Hello world!'
@@ -16,4 +17,12 @@ end
 
 get '/download/:filename' do |filename|
   send_file "./public/sites/#{filename}", :filename => filename, :type => 'Application/octet-stream'
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
 end
