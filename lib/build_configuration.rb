4881cf4389ba3ee79b30842423912c9e156d22d5
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-04-01 12:35:58 +0900

    Verification of value

--- lib/build_configuration.rb
+++ lib/build_configuration.rb
@@ -17,7 +17,7 @@ class BuildConfiguration
 
     def build_config_file(option = {}, path='./tmp/_config.yml')
       config = DEFAULTS.each_with_object({}) do |(key, val), arr|
-        arr[key] = if option.key?(key)
+        arr[key] = if option.key?(key) && !option[key].empty?
                      option[key]
                    else
                      val
